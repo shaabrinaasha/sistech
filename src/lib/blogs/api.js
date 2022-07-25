c8be9736aaa01c7a8f101ae418a4053ceab8cb73
Shabrina Nurmalitasari <shabrinanurmalitasari@gmail.com>
2022-07-25 13:45:30 +0700

    first commit

---
+++ src/lib/blogs/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const API_TOKEN="f862cd12-43c0-464c-aedc-5bd9699f88ed"
+const API='https://sistech-api.vercel.app/blog'
+
+export const getBlog = function () {
+    // axios.get(API, Headers: {"Authorization": `Bearer ${token}`})
+    return axios({
+        method: 'get',
+        url: API,
+        headers : {"Authorization": `Bearer ${API_TOKEN}`},
+      });
+
+} 
+
+export const updateBlog = function (blogData) {
+    return axios({
+        method: 'put',
+        url: API,
+        headers : {"Authorization": `Bearer ${API_TOKEN}`},
+        data: blogData,
+    }
+    )
+}
+
+export const likeBlog = function (id) {
+    return axios(
+        {
+            method: 'post',
+            url: `${API}/like`,
+            headers: {"Authorization": `Bearer ${API_TOKEN}`},
+            data: {
+                id: id,
+            }
+        }
+    )
+}
+
+
---
+++ src/routes/blogs/index.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { getBlog, likeBlog, updateBlog } from "$lib/blogs/api";
+  import { goto } from "$app/navigation";
+
+  /**
+   * @type {any[]}
+   */
+  let blogs = [];
+
+  const start = async function () {
+    const blogsResponses = await getBlog();
+    console.log(blogsResponses);
+    const blogData = blogsResponses.data;
+    for (var i = 0; i < blogData.length; i++) {
+      blogData[i].isUpdate = 0;
+      blogData[i].newTitle = "";
+      blogData[i].newContent = "";
+    }
+    blogs = blogsResponses.data;
+  };
+
+  let profilesPromise = start();
+
+  // @ts-ignore
+  const update = function (i) {
+    blogs[i].isUpdate = 1;
+    blogs[i].newTitle = blogs[i].title;
+    blogs[i].newContent = blogs[i].content;
+  };
+
+  const cancel = function (i, a) {
+    blogs[i].isUpdate = 0;
+  };
+
+  const submit = async function (i) {
+    var blogData = {
+      title: blogs[i].newTitle,
+      content: blogs[i].newContent,
+      id: blogs[i].id,
+    };
+
+    await updateBlog(blogData);
+    window.location.reload();
+  };
+
+  const like = async function (i) {
+    await likeBlog(blogs[i].id);
+    blogs[i].like += 1;
+  };
+</script>
+
+{#await profilesPromise}
+  <p>.. wait</p>
+{:then _}
+  {#each blogs as blog, i}
+    <div style="border: 3px solid lightblue; margin: 20px; padding: 20px">
+      {#if !blog.isUpdate}
+        <h1>{blog.title}</h1>
+        <p>{blog.content}</p>
+        <button on:click={() => update(i)}>Update</button>
+        <button on:click={() => like(i)}>Like ({blog.like})</button>
+      {:else}
+        <label for="name">Title</label> <br />
+        <input
+          bind:value={blogs[i].newTitle}
+          type="text"
+          id="title"
+          name="title"
+        /><br />
+        <label for="desc">Content</label><br />
+        <textarea
+          bind:value={blogs[i].newContent}
+          id="content"
+          name="content"
+        /><br />
+        <button on:click={() => cancel(i)}>Cancel</button>
+        <button on:click={() => submit(i)}>Submit</button>
+      {/if}
+    </div>
+  {/each}
+{/await}
